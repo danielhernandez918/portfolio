16041050234c84081c6478a75dc789692cf3d66b
Daniel Hernandez <danielhernandez918@gmail.com>
2022-10-18 18:20:12 -0700

    adding other projects into portfolio

--- src/views/projects/projects.jsx
+++ src/views/projects/projects.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import elden from "../../static/videos/elden.mp4";
 import alarm from "../../static/videos/alarm.mp4";
 import dnd from "../../static/videos/dnd.mp4";
+import portfolio from "../../static/videos/portfolio.mp4";
+import bugTracker from "../../static/videos/bugTracker.mp4";
 import { motion } from 'framer-motion'
 import { AiFillEye, AiFillGithub } from 'react-icons/ai'
 import './projectsStyles.scss';
@@ -101,6 +103,74 @@ const Projects = () => {
                                 </motion.div>
                             </a>
                             <a href={"https://github.com/danielhernandez918/DNDcommerce"} target="_blank" rel="noreferrer" >
+                                <motion.div
+                                        whileInView={{ scale: [0, 1] }}
+                                        whileHover={{ scale: [1, 2] }}
+                                        transition={{ duration: 0.25 }}
+                                >
+                                    <AiFillGithub/>
+                                </motion.div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </motion.div>
+            <motion.div 
+                // whileInView={{x: [150, 0], y: [-100, 0], opacity: [0, 1]}}
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duraction: 0.5 }} >
+                <h3 className="project-name">Portfolio</h3>
+                <div className="projectFlex">
+                    <video src={portfolio}  autoPlay loop muted/>
+                    <div className="space">
+                        <p className="projectDesc">Portfolio website showing my projects, skills, work experience, educations and more. </p>
+                        <br/>
+                        <p>Created using React-Native, Express, Flask, JSX, and Sass</p>
+                        <div className="icon-flex">
+                            <a href={"https://github.com/danielhernandez918/portfolio/blob/master/README.md"} target="_blank" rel="noreferrer" >
+                                <motion.div
+                                        whileInView={{ scale: [0, 1] }}
+                                        whileHover={{ scale: [1, 2] }}
+                                        transition={{ duration: 0.25 }}
+                                >
+                                    <AiFillEye/>
+                                </motion.div>
+                            </a>
+                            <a href={"https://github.com/danielhernandez918/portfolio/tree/master"} target="_blank" rel="noreferrer" >
+                                <motion.div
+                                        whileInView={{ scale: [0, 1] }}
+                                        whileHover={{ scale: [1, 2] }}
+                                        transition={{ duration: 0.25 }}
+                                >
+                                    <AiFillGithub/>
+                                </motion.div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </motion.div>
+            <motion.div 
+                // whileInView={{x: [150, 0], y: [-100, 0], opacity: [0, 1]}}
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duraction: 0.5 }} >
+                <h3 className="project-name">Oh Bugger Bug Tracker</h3>
+                <div className="projectFlex">
+                    <video src={bugTracker}  autoPlay loop muted/>
+                    <div className="space">
+                        <p className="projectDesc">Bug Tracker that allows teams to manage the tracking of bugs in their related projects. </p>
+                        <br/>
+                        <p>Created using Java, java Server Pages, MySQL, Bootstrap, CSS and many-to-many relationships </p>
+                        <div className="icon-flex">
+                            <a href={"https://github.com/danielhernandez918/bugTracker/blob/master/README.md"} target="_blank" rel="noreferrer" >
+                                <motion.div
+                                        whileInView={{ scale: [0, 1] }}
+                                        whileHover={{ scale: [1, 2] }}
+                                        transition={{ duration: 0.25 }}
+                                >
+                                    <AiFillEye/>
+                                </motion.div>
+                            </a>
+                            <a href={"https://github.com/danielhernandez918/bugTracker"} target="_blank" rel="noreferrer" >
                                 <motion.div
                                         whileInView={{ scale: [0, 1] }}
                                         whileHover={{ scale: [1, 2] }}
